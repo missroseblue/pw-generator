85fbc2d6c5bf52e39dd26f0912003daa76f9cc63
missroseblue <missroseblue@gmail.com>
2021-08-28 17:54:48 -0700

    Added clipboard functionality

--- assets/JS/generator.js
+++ assets/JS/generator.js
@@ -15,13 +15,14 @@ const randomFunc = {
     symbol: getRandomSymbol,
 };
 
+//Generate event listener
 generateEl.addEventListener('click', () => {
     const length = +lengthEl.value;
     const hasLower = lowercaseEl.checked;
     const hasUpper = uppercaseEl.checked;
     const hasNumber = numbersEl.checked;
     const hasSymbol = symbolsEl.checked;
-    
+
     resultEl.innerText = generatePassword(
         hasLower, 
         hasUpper, 
@@ -31,17 +32,63 @@ generateEl.addEventListener('click', () => {
         );
 });
 
+
+//Generate password function
+function generatePassword(lower, upper, number, symbol, length) {
+
+    let generatedPassword = '';
+
+    const typesCount = lower + upper + number + symbol;
+
+    const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(
+        item => Object.values(item)
+        );
+        if(typesCount === 0) {
+            return '';
+        }
+    for (let i = 0; i < length; i += typesCount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0];
+
+            generatedPassword += randomFunc[funcName]();
+
+        });
+    }
+
+   const finalPassword = generatedPassword.slice(0, length); {
+       return finalPassword;
+   }
+}
+
+//Copy password to clipboard
+clipboardEl.addEventListener('click'), () => {
+    const textarea = document.createElement('textarea');
+    const password = resultEl.innerText;
+
+    if(!password) {
+        return;
+    }
+
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+    alert('Password copied to clipboard!');
+};
+
+
 //generator functions
 function getRandomLower (){
-    return String.fromCharCode(Math.floor(Math.random() *26) +97);
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
 function getRandomUpper () {
-    return String.fromCharCode(Math.floor(Math.random() * 26) +65);
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
 function getRandomNumber () {
-    return String.fromCharCode(Math.floor(Math.random() *10) +48);
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
 function getRandomSymbol () {
